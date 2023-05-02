0fba019db39ab869c8e75b556282cb7f5237e908
Endrju00 <75850182+Endrju00@users.noreply.github.com>
2023-05-02 21:14:47 +0200

    Add tabs with checking and unchecking

--- Frontend Development/Angular/nodetodo/public/core.js
+++ Frontend Development/Angular/nodetodo/public/core.js
@@ -3,7 +3,8 @@ var nodeTodo = angular.module("nodeTodo", []);
 function mainController($scope, $http) {
   $scope.formData = {};
   $scope.todos = [];
-  $scope.cos = "Ala";
+  $scope.deleted = [];
+  $scope.cos = "Andrzej";
 
   // when landing on the page, get all todos and show them
   $http
@@ -30,7 +31,21 @@ function mainController($scope, $http) {
 
   // update a todo after checking it
   $scope.updateTodo = function(id) {
-    
+    let d = $scope.deleted.find((todo) => todo._id === id);
+    let index = $scope.deleted.indexOf(d);
+    if (index > -1) {
+      $scope.deleted.splice(index, 1);
+    }
+    $scope.todos.push(d);
+    $http
+      .post("/api/todos", {text: d.text})
+      .success(function(data) {
+        document.getElementById("newTodo").value = "";
+        $scope.todos = data;
+      })
+      .error(function(data) {
+        console.log("Error: " + data);
+      });
   };
 
   // delete a todo after checking it
@@ -38,6 +53,8 @@ function mainController($scope, $http) {
     $http
       .delete("/api/todos/" + id)
       .success(function(data) {
+        let d = $scope.todos.find((todo) => todo._id === id);
+        $scope.deleted.push(d);
         $scope.todos = data;
       })
       .error(function(data) {
